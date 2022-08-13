f0ebab2f7dfe4d90f600b039c532e55100e094ba
CJM <whdals9514@naver.com>
2022-08-13 17:09:23 +0900

    test

---
+++ app/models/index.js
@@ -0,0 +1,23 @@
+const dbconfig = require('../config/db.config.js');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(dbconfig.DB, dbconfig.USER, dbconfig.PASSWORD, {
+    host: dbconfig.HOST,
+    dialect: dbconfig.dialect,
+
+    pool: {
+        max: dbconfig.pool.max,	
+   		min: dbconfig.pool.min,
+      	acquire: dbconfig.pool.acquire,
+      	idle: dbconfig.pool.idle
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
+module.exports = db;
